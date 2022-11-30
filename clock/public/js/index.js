38d4f227d34f27e1790d20fa8d4632badfe05b17
ADDFa <fratamaadha18@gmail.com>
2022-11-30 16:02:19 +0700

    benerin bug di hour

--- clock/public/js/index.js
+++ clock/public/js/index.js
@@ -6,13 +6,8 @@ runClock = () => {
 
     document.querySelector('.second').style.transform = `rotate(${6 * second}deg)`
     document.querySelector('.minute').style.transform = `rotate(${6 * minute}deg)`
-<<<<<<< HEAD
-    document.querySelector('.hour').style.transform = `rotate(${hour / 4 * 60}deg)`
+    document.querySelector('.hour').style.transform = `rotate(${hour / 8 * 60}deg)`
     document.querySelector('.text-times span').textContent = `${hour} : ${minute} : ${(second < 10) ? '0' + second : second}`
-=======
-    document.querySelector('.hour').style.transform = `rotate(${hour / 2 * 30}deg)`
-    document.querySelector('.text-times span').textContent = `${(hour < 10) ? '0' + hour : hour} : ${(minute < 10) ? '0' + minute : minute} : ${(second < 10) ? '0' + second : second}`
->>>>>>> a9643c9de6e1a88bc57f64b2ace1ea1752d49e18
 }
 
 setInterval(runClock, 1000)
