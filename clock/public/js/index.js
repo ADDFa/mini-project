31438bd01e75e890d7f7256508ce7b227616323f
ADDFa <fratamaadha18@gmail.com>
2022-11-30 18:57:55 +0700

    fixing bug

--- clock/public/js/index.js
+++ clock/public/js/index.js
@@ -1,13 +1,26 @@
+const getHour = ({ hour, minute }) => {
+    let hours = hour
+    if (hour > 12) hours = hour - 12
+    let result = hours * 30 + (minute / 2)
+
+    return result
+}
+
 runClock = () => {
     const date = new Date
-    const hour = date.getHours()
     const minute = date.getMinutes()
     const second = date.getSeconds()
+    const hours = {
+        hour: date.getHours(),
+        minute
+    }
+
+    const hour = getHour(hours)
 
     document.querySelector('.second').style.transform = `rotate(${6 * second}deg)`
     document.querySelector('.minute').style.transform = `rotate(${6 * minute}deg)`
-    document.querySelector('.hour').style.transform = `rotate(${hour / 8 * 60}deg)`
-    document.querySelector('.text-times span').textContent = `${(hour < 10) ? '0' + hour : hour} : ${(minute < 10) ? '0' + minute : minute} : ${(second < 10) ? '0' + second : second}`
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+    document.querySelector('.text-times span').textContent = `${(hours.hour < 10) ? '0' + hours.hour : hours.hour} : ${(minute < 10) ? '0' + minute : minute} : ${(second < 10) ? '0' + second : second}`
 }
 
 setInterval(runClock, 1000)
