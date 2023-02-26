755266e6ae982049601492bd9a53d6bb91a1f2d9
ADDFa <fratamaadha18@gmail.com>
2023-02-26 10:08:47 +0700

    add git ignore

---
+++ todolist/assets/js/script.js
@@ -0,0 +1,23 @@
+import { el } from "./Dom/_function.js";
+import { instanceTodo } from "./TodoStorage/Todo.js";
+import { setPopupContent, openPopup } from "./Dom/_popup.js";
+import { contentPopup as contentPopupNewTodolist, eventCreateTodo } from "./Dom/_newTodolist.js";
+import setTodolist from "./Dom/_setTodolist.js";
+import "./Dom/_deleteTodolist.js";
+const todolistAction = el(".todolist-action");
+document.addEventListener("DOMContentLoaded", () => setTodolist());
+todolistAction.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("new-todolist")) {
+        // set popup content
+        setPopupContent(contentPopupNewTodolist, eventCreateTodo);
+        // munculkan popup
+        openPopup();
+    }
+    // remove all todolist
+    if (target.classList.contains("remove-all-todolist")) {
+        if (confirm("Anda Yakin?").valueOf()) {
+            instanceTodo.deleteAllTodo();
+            setTodolist();
+        }
+    }
+});
